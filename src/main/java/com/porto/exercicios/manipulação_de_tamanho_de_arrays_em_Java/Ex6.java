bf3222cc787d7f252a0f2cb12cc5a2bb4d391c42
Leo Porto <leonardoreisporto@gmail.com>
2024-08-26 07:51:48 -0300

    exercicios

--- src/main/java/com/porto/exercicios/manipulação_de_tamanho_de_arrays_em_Java/Ex6.java
+++ src/main/java/com/porto/exercicios/manipulação_de_tamanho_de_arrays_em_Java/Ex6.java
@@ -1,8 +1,18 @@
 package com.porto.exercicios.manipulação_de_tamanho_de_arrays_em_Java;
 
+import java.util.Arrays;
+
 public class Ex6 {
     public static void main(String[] args) {
         int[] inteiros = {10, 20, 30};
+        int[] clone = Arrays.copyOf(inteiros, 6);
+
+        clone[3] = 40;
+        clone[4] = 50;
+        clone[5] = 60;
+
+        System.out.println(Arrays.toString(inteiros));
+        System.out.println(Arrays.toString(clone));
     }
 }
 
