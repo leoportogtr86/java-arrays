3ed8a5e1c63a685732c72fe2cf73393309ffa3e5
Leo Porto <leonardoreisporto@gmail.com>
2024-08-26 07:22:19 -0300

    Clonando Arrays em Java

---
+++ src/main/java/com/porto/exercicios/manipulação_de_tamanho_de_arrays_em_Java/Ex6.java
@@ -0,0 +1,12 @@
+package com.porto.exercicios.manipulação_de_tamanho_de_arrays_em_Java;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        int[] inteiros = {10, 20, 30};
+    }
+}
+
+//6. **Redimensionamento Manual**:
+//- Crie um array de 3 inteiros, inicialize-o com valores de sua escolha,
+//e depois redimensione-o para 6 elementos,
+//mantendo os valores originais e adicionando novos valores.
